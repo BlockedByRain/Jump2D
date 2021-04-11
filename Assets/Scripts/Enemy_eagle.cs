e040eb2b3b1b69537167fe3bee9a6e73fa9476e6
独街雨封 <1596127831@qq.com>
2021-04-12 01:58:58 +0800

    202104120 0158
    睡觉

---
+++ Assets/Scripts/Enemy_eagle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_eagle : Enemy
+{
+    private Rigidbody2D rb;
+    //private Collider2D coll;
+    public Transform top, bottom;
+    public float speed;
+    private float topY, bottomY;
+
+
+    private bool isUp;
+
+
+
+
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+        //coll = GetComponent<Collider2D>();
+        topY = top.position.y;
+        bottomY = bottom.position.y;
+        Destroy(top.gameObject);
+        Destroy(bottom.gameObject);
+
+
+
+    }
+
+
+    void Update()
+    {
+        Movement();
+    }
+
+
+
+    void Movement()
+    {
+        if (isUp)
+        {
+            rb.velocity = new Vector2(rb.velocity.x,speed);
+            if (transform.position.y>topY)
+            {
+                isUp = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -speed);
+            if (transform.position.y < bottomY)
+            {
+                isUp = true;
+            }
+        }
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/Enemy_frog.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_frog : Enemy
+{
+    public Transform leftPoint, rightPoint;
+    public float speed,jumpForce;
+    public LayerMask ground;
+
+    private Rigidbody2D rb;
+    //private Animator anim;
+    private Collider2D coll;
+    private float leftX, rightX;
+    private bool faceLeft = true;
+
+
+
+    protected override void Start()
+    {
+        base.Start();
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+
+        leftX = leftPoint.position.x;
+        rightX = rightPoint.position.x;
+        transform.DetachChildren();
+        Destroy(leftPoint.gameObject);
+        Destroy(rightPoint.gameObject);
+
+    }
+
+    void Update()
+    {
+        SwitchAnim();
+    }
+
+
+    void Movement()//移动
+    {
+        if (faceLeft)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                anim.SetBool("jumping", true);
+                rb.velocity = new Vector2(-speed, jumpForce);
+            }        
+            if (transform.position.x < leftX)
+            {
+                rb.velocity = new Vector2(speed, jumpForce);
+                transform.localScale = new Vector3(-1, 1, 1);
+                faceLeft = false;
+            }
+        }
+        else
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                anim.SetBool("jumping", true);
+                rb.velocity = new Vector2(speed, jumpForce);
+            }
+            if (transform.position.x > rightX)
+            {
+                rb.velocity = new Vector2(-speed, jumpForce);
+                transform.localScale = new Vector3(1, 1, 1);
+                faceLeft = true;
+            }
+        }
+    }
+
+
+    void SwitchAnim()//动画改变
+    {
+        if (anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y<0.1f)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        if (coll.IsTouchingLayers(ground) && anim.GetBool("falling"))
+        {
+            anim.SetBool("falling", false);
+        }
+    }
+
+
+
+}
